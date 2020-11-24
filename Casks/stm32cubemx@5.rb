a3f45e9b7ece14713e90e2786fdf94d4d356d177
Ladislas de Toldi <ladislas@detoldi.me>
2020-11-24 22:15:58 +0100

    Improve v5.6.1 cask

--- Casks/stm32cubemx@5.rb
+++ Casks/stm32cubemx@5.rb
@@ -1,4 +1,26 @@
-cask 'stm32cubemx' do
+cask 'stm32cubemx@5' do
+	module Utils
+		def self.version_suffix
+			 return "@5"
+		end
+
+		def self.cubemx_original_app_path
+			 return "/Applications/STMicroelectronics/STM32CubeMX.app"
+		end
+
+		def self.cubemx_final_app_path
+			 return "/Applications/STMicroelectronics/STM32CubeMX#{self.version_suffix}.app"
+		end
+
+		def self.usr_local_bin_file
+			 return "/usr/local/bin/stm32cubemx#{self.version_suffix}"
+		end
+
+		def self.cubemx_script_file
+			 return "#{self.cubemx_final_app_path}/Contents/MacOs/stm32cubemx.sh"
+		end
+	end
+
   version '5.6.1'
   sha256 'ff3ed237790529b2134f2254789220d7def05669857d5e0e1e60d387c17ab4bb'
 
@@ -14,37 +36,29 @@ cask 'stm32cubemx' do
     must_succeed: true
 	}
 
-	version_suffix = '@5'
-
-	cubemx_original_app_path = "#{appdir}/STMicroelectronics/STM32CubeMX.app"
-	cubemx_final_app_path = "#{appdir}/STMicroelectronics/STM32CubeMX#{version_suffix}.app"
-
-	usr_local_bin_file = "/usr/local/bin/stm32cubemx#{version_suffix}"
-	cubemx_script_file = "#{cubemx_final_app_path}/Contents/MacOs/stm32cubemx.sh"
-
 	postflight do
-    unless version_suffix.empty? do
-      ohai "Adding #{version_suffix} to STM32CubeMX.app filename"
-      FileUtils.mv cubemx_original_app_path cubemx_final_app_path
+    unless Utils.version_suffix.empty?
+      ohai "Adding #{Utils.version_suffix} to STM32CubeMX.app filename"
+      FileUtils.mv Utils.cubemx_original_app_path Utils.cubemx_final_app_path
 		end
 
-    File.open(cubemx_script_file, "w") { |f|
-      f.write("#!/usr/bin/env bash\njava -jar #{cubemx_final_app_path}/Contents/MacOs/STM32CubeMX $@\n")
+    File.open(Utils.cubemx_script_file, "w") { |f|
+      f.write("#!/usr/bin/env bash\njava -jar #{Utils.cubemx_final_app_path}/Contents/MacOs/STM32CubeMX $@\n")
       f.close
     }
 
-    FileUtils.chmod 0755, cubemx_script_file
-    FileUtils.ln_sf cubemx_script_file, usr_local_bin_file
+    FileUtils.chmod 0755, Utils.cubemx_script_file
+    FileUtils.ln_sf Utils.cubemx_script_file, Utils.usr_local_bin_file
   end
 
   uninstall script: {
     executable: 'java',
-    args:       ['-jar', '#{cubemx_final_app_path}/Contents/Resources/Uninstaller/uninstaller.jar'],
+    args:       ['-jar', '#{Utils.cubemx_final_app_path}/Contents/Resources/Uninstaller/uninstaller.jar'],
     must_succeed: true
   }
 
   uninstall_preflight do
-    FileUtils.rm usr_local_bin_file, force: true
+    FileUtils.rm Utils.usr_local_bin_file, force: true
   end
 
   caveats do
